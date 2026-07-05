42b3f37489c5237c68542e4b44b8c896cb52720e
carolOriama <carolineoriama19@outlook.com>
2026-07-05 22:59:49 +0300

    Added HTTP methods and validated requests

--- app.py
+++ app.py
@@ -30,6 +30,85 @@ def fetch_product(barcode):
 
     return None
 
+products = [
+         {
+        "id": 1,
+        "barcode": "025293001225",
+        "product_name": "Organic Almond Milk",
+        "brand": "Silk",
+        "ingredients": "Filtered water, almonds, cane sugar...",
+        "price": 3.99,
+        "quantity": 50
+    }
+]
+
+
+"""Implementing CRUD"""
+
+@app.route("/inventory", methods=["GET"])
+def get_all_products():
+    return jsonify(list(products.values())),200
+
+@app.route("/inventory/<int:id>", methods = ["GET"])
+def get_product(id):
+    product = products.get(id)
+    if product:
+        return jsonify(product), 200
+    return jsonify({"Product not found"}), 404
+
+@app.route("/inventory", methods= ["POST"])
+def create_product(id):
+    data = request.get_json()
+    if not data or "barcode" not in data or "price" not in data or "quantity" not in data:
+        return jsonify({"error": "Missing required fields: barcode, price, quantity"}), 400
+    
+    barcode = data["barcode"]
+
+    external_data = fetch_product(barcode)
+    if not external_data:
+        return jsonify({"error": f"Product with barcode {barcode} could not be found on OpenFoodFacts."}), 422
+    
+    new_id = max(products.keys()) + 1 if products else 1
+
+    new_item = {
+        "id": new_id,
+        "barcode": barcode,
+        "product_name": external_data["product_name"],
+        "brand": external_data["brand"],
+        "ingredients": external_data["ingredients"],
+        "price": float(data["price"]),
+        "quantity": int(data["quantity"])
+    }
+
+    products[new_id] = new_item
+    return jsonify(new_item), 201
+
+@app.route("/inventory/<int:id>", methods=["PATCH"])
+def update_item(id):
+    item = products.get(id)
+    if not item:
+        return jsonify({"Item not found"}), 404
+        
+    data = request.get_json()
+    if not data:
+        return jsonify({"No data provided"}), 400
+
+    if "price" in data:
+        item["price"] = float(data["price"])
+    if "quantity" in data:
+        item["quantity"] = int(data["quantity"])
+        
+    return jsonify(item), 200
+
+
+@app.route("/inventory/<int:id>", methods=["DELETE"])
+def delete_item(id):
+    global products
+    if id in products:
+        del products[id]
+        return jsonify({"message": f"Item {id} successfully deleted"}), 200
+    return jsonify({"Item not found"}), 404
+
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
