6290a150c021360c35ba3c08d8a4337eaf5f8ed4
carolOriama <carolineoriama19@outlook.com>
2026-07-05 22:12:20 +0300

    Implemented API to extract product fields

--- app.py
+++ app.py
@@ -1,8 +1,35 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify
+import requests
 
 app = Flask(__name__)
 
 
+def fetch_product(barcode):
 
-if __name__== "__main__":
+    url = f"https://world.openfoodfacts.org/api/v0/product/{barcode}.json"
+    
+    try:
+        #Add header so that api knows who is calling it.
+        headers = {'User-Agent': 'InventoryManagerApp - WebCourseProject - Version 1.0'}
+        response = requests.get(url, headers=headers, timeout=5)
+        
+        if response.status_code == 200:
+            data = response.json()
+            
+            if data.get("status") == 1:
+                product_info = data.get("product", {})
+                
+                return {
+                    "product_name": product_info.get("product_name", "None"),
+                    "brand": product_info.get("brands", "None"),
+                    "ingredients": product_info.get("ingredients_text", "None.")
+                }
+    except requests.exceptions.RequestException as e:
+        print(f"External API Error: {e}")
+
+
+    return None
+
+
+if __name__=="__main__":
     app.run(debug=True)
