d93695fd10557f16f42285efc8ad5600f7a308d1
carolOriama <carolineoriama19@outlook.com>
2026-07-07 18:20:46 +0300

    setting up test client fixtures and implement GET routes unit tests

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,3 +2,22 @@ import pytest
 from unittest.mock import patch, Mock
 from app import app, products
 
+@pytest.fixture
+def client():
+    
+    app.config["TESTING"] = True
+    with app.test_client() as client:
+        yield client
+
+@pytest.fixture(autouse=True)
+def reset_database():
+    products.clear()
+    products[1] = {
+        "id": 1,
+        "barcode": "025293001225",
+        "product_name": "Organic Almond Milk",
+        "brand": "Silk",
+        "ingredients": "Filtered water, almonds, cane sugar...",
+        "price": 3.99,
+        "quantity": 50
+    }
